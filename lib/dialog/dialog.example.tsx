0ecabf76d8e2550c0591414e83da8cc520b280fe
terremou <terrence_mou@foxmail.com>
2022-01-05 14:58:55 +0800

    给 dialog 添加事件处理

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -4,15 +4,41 @@ import {Button} from '../button/button';
 
 const DialogExample: React.FC = () => {
   const [v, setV] = useState(false);
+  const [c, setC] = useState(false);
   return (
     <>
-      <Button onClick={() => setV(!v)}>click</Button>
-      <Dialog visible={v}>
-        <strong>hi</strong>
-      </Dialog>
+      <div>
+        <h1>example 1</h1>
+        <Button onClick={() => setV(!v)}>click</Button>
+        <Dialog visible={v}
+                onClose={() => setV(false)}
+                button={
+                  [
+                    <Button onClick={() => setV(false)}>取消</Button>,
+                    <Button onClick={() => setV(false)}>确认</Button>
+                  ]
+                }>
+          <strong>example1</strong>
+        </Dialog>
+      </div>
+      <div>
+        <h1>example 2</h1>
+        <Button onClick={() => setC(!c)}>click</Button>
+        <Dialog visible={c}
+                closeOnClickMask={true}
+                onClose={() => setC(false)}
+                button={
+                  [
+                    <Button onClick={() => setC(false)}>取消</Button>,
+                    <Button onClick={() => setC(false)}>确认</Button>
+                  ]
+                }>
+          <strong>example2</strong>
+        </Dialog>
+      </div>
     </>
-)
-  ;
+  )
+    ;
 };
 
 export {DialogExample};
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,37 +1,56 @@
-import React from 'react';
-import './dialog.scss'
+import React, {ReactElement} from 'react';
+import './dialog.scss';
 import {Icon} from '../index';
 import {scopedClassMaker} from '../scopedClassMaker';
-import {Button} from '../button/button';
 
-const scopedClass = scopedClassMaker('hugsyui-dialog')
-const sc = scopedClass
+const scopedClass = scopedClassMaker('hugsyui-dialog');
+const sc = scopedClass;
 
 interface Props {
   visible: boolean;
+  button: Array<ReactElement>;
+  onClose: React.MouseEventHandler;
+  closeOnClickMask?: boolean;
 }
+
 const Dialog: React.FC<Props> = (props) => {
+  const onClickClose: React.MouseEventHandler = (e) => {
+    props.onClose(e);
+  };
+  const onClickMask: React.MouseEventHandler = (e) => {
+    if (props.closeOnClickMask) {
+      props.onClose(e);
+    }
+  };
 
   return (
     props.visible ?
       <>
-        <div className={sc('mask')}/>
+        <div className={sc('mask')}
+             onClick={onClickMask}/>
         <div className={sc()}>
-          <div className={sc('close')}>
-            <Icon name='close'/>
+          <div className={sc('close')}
+               onClick={onClickClose}
+          >
+            <Icon name="close"/>
           </div>
           <header className={sc('header')}>提示</header>
           <main className={sc('main')}>
             {props.children}
           </main>
           <footer className={sc('footer')}>
-            <Button>取消</Button>
-            <Button>确认</Button>
+            {props.button.map((btn, index) =>
+              React.cloneElement(btn, {key: index})
+            )}
           </footer>
         </div>
       </> :
       null
   );
+};
+
+Dialog.defaultProps = {
+  closeOnClickMask: false
 };
 
 export {Dialog};
