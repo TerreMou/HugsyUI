361abe422db6d60c02fa9b55ac8b0f81c85a7656
terremou <terrence_mou@foxmail.com>
2022-01-06 18:09:45 +0800

    完成四个基本布局

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -3,16 +3,53 @@ import {Layout} from './layout';
 import {Header} from './header';
 import {Content} from './content';
 import {Footer} from './footer';
+import {Aside} from './aside';
 
 const LayoutExample: React.FC = () => {
   return (
     <>
       <div>
         <h1>example 1</h1>
-        <Layout className='x'>
-          <Header className='y'>header</Header>
-          <Content className='z'>content</Content>
-          <Footer className='k'>footer</Footer>
+        <Layout className="x" style={{height: 250}}>
+          <Header className="y">header</Header>
+          <Content className="z">content</Content>
+          <Footer className="k">footer</Footer>
+        </Layout>
+      </div>
+
+      <div>
+        <h1>example 2</h1>
+        <Layout className="x" style={{height: 250}}>
+          <Header className="y">header</Header>
+          <Layout>
+            <Aside className="j">aside</Aside>
+            <Content className="z">content</Content>
+          </Layout>
+          <Footer className="k">footer</Footer>
+        </Layout>
+      </div>
+
+      <div>
+        <h1>example 3</h1>
+        <Layout className="x" style={{height: 250}}>
+          <Header className="y">header</Header>
+          <Layout>
+            <Content className="z">content</Content>
+            <Aside className="j">aside</Aside>
+          </Layout>
+          <Footer className="k">footer</Footer>
+        </Layout>
+      </div>
+
+      <div>
+        <h1>example 4</h1>
+        <Layout className="x" style={{height: 250}}>
+          <Aside>aside</Aside>
+          <Layout>
+            <Header className="y">header</Header>
+            <Content className="z">content</Content>
+            <Footer className="k">footer</Footer>
+          </Layout>
         </Layout>
       </div>
 
--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import {classes} from '../helpers/scopedClassMaker';
+import './layout.scss'
+import {Aside} from './aside';
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
+  children: ReactElement | Array<ReactElement>
 }
 
 const Layout: React.FC<Props> = ({className, children, ...rest}) => {
+  let hasAside = false;
+  if ((children as Array<ReactElement>).length) {
+    (children as Array<ReactElement>).map(node=>{
+      if (node.type === Aside) {
+        hasAside = true
+      }
+    })
+  }
+
   return (
-    <div className={classes('hugsyui-layout', className)} {...rest}>
+    <div className={classes('hugsyui-layout', [className,hasAside && 'hugsyui-layout-hasAside'].join(' ') )} {...rest}>
       {children}
     </div>
   );
