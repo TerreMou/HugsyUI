83f8b12ba0989bcb6a2878e971e5d072f52df04f
terremou <terrence_mou@foxmail.com>
2022-01-06 20:31:26 +0800

    完成 layout 组件

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -4,51 +4,52 @@ import {Header} from './header';
 import {Content} from './content';
 import {Footer} from './footer';
 import {Aside} from './aside';
+import './layout.example.scss'
 
 const LayoutExample: React.FC = () => {
   return (
     <>
       <div>
         <h1>example 1</h1>
-        <Layout className="x" style={{height: 250}}>
-          <Header className="y">header</Header>
-          <Content className="z">content</Content>
-          <Footer className="k">footer</Footer>
+        <Layout style={{height: 250, width: 800}}>
+          <Header className="header">header</Header>
+          <Content className="content">content</Content>
+          <Footer className="footer">footer</Footer>
         </Layout>
       </div>
 
       <div>
         <h1>example 2</h1>
-        <Layout className="x" style={{height: 250}}>
-          <Header className="y">header</Header>
+        <Layout style={{height: 250, width: 800}}>
+          <Header className="header">header</Header>
           <Layout>
-            <Aside className="j">aside</Aside>
-            <Content className="z">content</Content>
+            <Aside className="aside">aside</Aside>
+            <Content className="content">content</Content>
           </Layout>
-          <Footer className="k">footer</Footer>
+          <Footer className="footer">footer</Footer>
         </Layout>
       </div>
 
       <div>
         <h1>example 3</h1>
-        <Layout className="x" style={{height: 250}}>
-          <Header className="y">header</Header>
+        <Layout style={{height: 250, width: 800}}>
+          <Header className="header">header</Header>
           <Layout>
-            <Content className="z">content</Content>
-            <Aside className="j">aside</Aside>
+            <Content className="content">content</Content>
+            <Aside className="aside">aside</Aside>
           </Layout>
-          <Footer className="k">footer</Footer>
+          <Footer className="footer">footer</Footer>
         </Layout>
       </div>
 
       <div>
         <h1>example 4</h1>
-        <Layout className="x" style={{height: 250}}>
-          <Aside>aside</Aside>
+        <Layout style={{height: 250, width: 800}}>
+          <Aside className="aside">aside</Aside>
           <Layout>
-            <Header className="y">header</Header>
-            <Content className="z">content</Content>
-            <Footer className="k">footer</Footer>
+            <Header className="header">header</Header>
+            <Content className="content">content</Content>
+            <Footer className="footer">footer</Footer>
           </Layout>
         </Layout>
       </div>
