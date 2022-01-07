dd89dc4ed3928d257097d5987b07f1866e684dfa
terremou <terrence_mou@foxmail.com>
2022-01-07 15:44:43 +0800

    新增 yarn doc 配置

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -1,24 +1,13 @@
 const base = require('./webpack.config');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = Object.assign({}, base, {
   mode: 'production',
   entry: {
-    ...base.entry,
     example: './example.tsx',
   },
-  externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React',
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDOM',
-    },
+  output: {
+    path: path.resolve(__dirname, 'doc'),
   },
   plugins: [
     new HtmlWebpackPlugin({
